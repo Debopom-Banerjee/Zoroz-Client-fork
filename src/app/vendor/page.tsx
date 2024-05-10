034cbca91d84f2aaca6f6f811615842a44db8c27
soumyarajbag <soumyarajbag@gmail.com>
2024-05-10 15:03:30 +0530

    done

--- src/app/vendor/page.tsx
+++ src/app/vendor/page.tsx
@@ -1,8 +1,32 @@
+"use client"
 import React from 'react'
 import { FaBusinessTime, FaShoppingCart, FaUser } from 'react-icons/fa'
 import { MdBrandingWatermark, MdOutlineCategory } from 'react-icons/md'
 import { RiProductHuntFill } from 'react-icons/ri'
+import { Doughnut, Line } from "react-chartjs-2";
 
+import {
+  Chart as ChartJS,
+  LineElement,
+  CategoryScale, // x axis
+  LinearScale, // y axis
+  PointElement,
+  Legend,
+  Tooltip,
+  Filler,
+  ArcElement,
+} from "chart.js";
+
+ChartJS.register(
+  ArcElement,
+  LineElement,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  Legend,
+  Tooltip,
+  Filler
+);
 const InfoCard = ({ title, value, color,icon }: { 
   title: string, 
   value: string,
@@ -29,6 +53,86 @@ const adminCards = [
 ]
 
 const page = () => {
+  const salesData = [
+    { month: "January", sales: 100 },
+    { month: "February", sales: 150 },
+    { month: "March", sales: 200 },
+    { month: "April", sales: 120 },
+    { month: "May", sales: 180 },
+    { month: "June", sales: 250 },
+  ];
+  const data = {
+    labels: salesData.map((data) => data.month),
+    datasets: [
+      {
+        label: "Count",
+        data: salesData.map((data) => data.sales),
+        borderColor: "#cb0c9f",
+        borderWidth: 3,
+        pointBorderColor: "#cb0c9f",
+        pointBorderWidth: 3,
+        tension: 0.5,
+        fill: true,
+        backgroundColor: (context:any) => {
+          const ctx = context.chart.ctx;
+          const gradient = ctx.createLinearGradient(0, 0, 0, 300);
+          gradient.addColorStop(0, "#f797e1");
+          gradient.addColorStop(1, "white");
+          return gradient;
+        },
+      },
+    ],
+  };
+
+  const options = {
+    plugins: {
+      legend: true,
+    },
+    responsive: true,
+    scales: {
+      y: {
+        ticks: {
+          font: {
+            size: 12,
+            weight: "bold",
+          },
+        },
+        title: {
+          display: true,
+          text: "Orders",
+          padding: {
+            bottom: 5,
+          },
+          font: {
+            size: 12,
+            style: "italic",
+            family: "Arial",
+          },
+        },
+        min: 50,
+      },
+      x: {
+        ticks: {
+          font: {
+            size: 12,
+            weight: "bold",
+          },
+        },
+        title: {
+          display: true,
+          text: "Month",
+          padding: {
+            top: 10,
+          },
+          font: {
+            size: 10,
+            style: "italic",
+            family: "Arial",
+          },
+        },
+      },
+    },
+  };
   return (
     <div className='my-5 flex flex-row flex-wrap w-full items-center gap-20 px-10'>
       {
@@ -36,6 +140,15 @@ const page = () => {
           <InfoCard key={index} title={card.title} value={card.value} color={card.color} icon={card.icon} />
         ))
       }
+      <h1 className='font-semibold text-xl'>Analytics</h1>
+    <div className='flex flex-row flex-wrap items-center gap-5 w-full justify-evenly'>
+    <div className='flex flex-col items-start h-full w-full lg:w-[40%]'>
+      <Line data={data}
+      // @ts-ignore
+      options={options}></Line>
+      </div>
+     
+    </div>
     </div>
   )
 }
