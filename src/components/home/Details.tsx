734fe496d9a74e712308eec5452b89d3cb240675
Debopom Banerjee <94488557+Debopom-Banerjee@users.noreply.github.com>
2024-05-24 08:17:53 +0530

    Update Details.tsx

--- src/components/home/Details.tsx
+++ src/components/home/Details.tsx
@@ -4,12 +4,11 @@ const SubDetail = () => {
   return (
     <div className="flex flex-col items-start gap-4 ">
       <h1 className="font-semibold text-sm">
-        Zoroz – Leading B2B e-Commerce for Industrial Products
+        ZOROZ – Your Trusted B2B Partner for Mobile Products.
       </h1>
       <p className="text-xs">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure
-        repellendus, earum cum quas ex voluptatibus aliquid voluptatem repellat
-        odit placeat.
+        ZOROZ is committed to streamlining the procurement of industrial products, ensuring a hassle-free online buying experience. Our platform aims to help you save on supplies, enhance inventory management efficiency, and sustain growth. We make B2B and B2C procurement smooth and time-saving.
+
       </p>
     </div>
   );
