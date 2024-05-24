e6029637906d8a0674a3bf44c8884d89dcfa5ef2
Debopom Banerjee <94488557+Debopom-Banerjee@users.noreply.github.com>
2024-05-24 07:27:48 +0530

    Add files via upload

---
+++ src/app/refund-and-returns-policy/page.tsx
@@ -0,0 +1,228 @@
+import Navbar from "@/components/common/Navbar";
+import Footer from "@/components/home/Footer";
+import React from "react";
+
+const page = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="flex flex-col justify-center items-center gap-5 mx-auto bg-white p-10 rounded-xl lg:w-3/5 my-10">
+        <h1 className="font-semibold text-4xl text-center w-full">
+          Refund and Cancellation Policy
+        </h1>
+        <div className="text-justify w-full">
+          Our focus is complete customer satisfaction. In the event, if you are
+          displeased with the services provided, we will refund back the money,
+          provided the reasons are genuine and proved after investigation.
+          Please read the fine prints of each deal before buying it, it provides
+          all the details about the services or the product you purchase. In
+          case of dissatisfaction from our services, clients have the liberty to
+          cancel their projects and request a refund from us. Our Policy for the
+          cancellation and refund will be as follows:
+          <h6>Damage/Warranty/Guarantee</h6>
+          Incase of physical damage, warranty, gaurantee or other replacement
+          condition of the product transportation charges will not be provided
+          by the company. The same must be bear by the customer himself/herself.
+          <h6>Cancellation Policy</h6>
+          For Cancellations please contact us via contact us link. 1. Any user
+          can cancel the order any time till the order status remains “pending”
+          or “on review” only. <br />
+          2. Only registered customer can cancel the order but order cancel
+          option is not available with guest checkout.
+          <br />
+          3. Requests received later than 7 business days prior to the end of
+          the current service period will be treated as cancellation of services
+          for the next service period.
+          <br />
+          <h5>Refund Policy</h5>
+          Back to Source(Refund in Bank Account) : In case customer wants to get
+          the refund amount back to the source (Bank Account), there will be a
+          deduction of 3% of the transactional value. Processing time 5-10
+          working days. 1. We will try our best to create the suitable design
+          concepts for our clients.
+          <br />
+          2. In case any client is not completely satisfied with our products we
+          can provide a refund.
+          <br />
+          3. After successful cancellation of any order, refund will done within
+          7-10 business working days from the date of cancellation.
+          <br />
+          4. If paid by credit card, refunds will be issued to the original
+          credit card provided at the time of purchase and in case of payment
+          gateway name payments refund will be made to the same account.
+          <br />
+          5. Privacy Policy: The terms “We” / “Us” / “Our”/”Company”
+          individually and collectively refer to ZOROZ and the terms “You”
+          /”Your” / “Yourself” refer to the users or customers.
+          <br />
+          6. This Privacy Policy is an electronic record in the form of an
+          electronic contract formed under the information Technology Act, 2000
+          and the rules made there under and the amended provisions pertaining
+          to electronic documents / records in various statutes as amended by
+          the information Technology Act, 2000. This Privacy Policy does not
+          require any physical, electronic or digital signature.
+          <br />
+          7. This Privacy Policy is a legally binding document between you and
+          ZOROZ (both terms defined below). The terms of this Privacy Policy
+          will be effective upon your acceptance of the same (directly or
+          indirectly in electronic form, by clicking on the I accept tab or by
+          use of the website or by other means) and will govern the relationship
+          between you and ZOROZ for your use of the website “Website” (defined
+          below).
+          <br />
+          8. This document is published and shall be construed in accordance
+          with the provisions of the Information Technology (reasonable security
+          practices and procedures and sensitive personal data of information)
+          rules, 2011 under Information Technology Act, 2000; that require
+          publishing of the Privacy Policy for collection, use, storage and
+          transfer of sensitive personal data or information.
+          <br />
+          9. Please read this Privacy Policy carefully by using the Website, you
+          indicate that you understand, agree and consent to this Privacy
+          Policy. If you do not agree with the terms of this Privacy Policy,
+          please do not use this Website.
+          <br />
+          10.By providing us your Information or by making use of the facilities
+          provided by the Website, You hereby consent to the collection,
+          storage, processing and transfer of any or all of Your Personal
+          Information and Non-Personal Information by us as specified under this
+          Privacy Policy. You further agree that such collection, use, storage
+          and transfer of Your Information shall not cause any loss or wrongful
+          gain to you or any other person.
+          <br />
+          Our company is not liable if the package is damaged or confiscated
+          during customs clearance. Please check your country’s customs policies
+          before placing an order. We are not responsible for damaged packages
+          or those returned due to courier service failures after multiple
+          delivery attempts (due to customer absence).
+          <h6>User Information</h6>
+          You are responsible for ensuring that your login credentials (your
+          username and password) are kept confidential. In order to provide
+          security measures, we have implemented technical and organizational
+          measures specially designed to secure your personal information from
+          accidental loss and unauthorized access, use, alteration or
+          disclosure. However, due to the inherent open nature of the Internet
+          and wireless communications, we do not provide guarantee that your
+          personal information will be completely free from unauthorized access
+          by third parties, because the transferring of money over or through
+          Zoroz’s Platforms are not within our exclusive control. Use of Zoroz
+          services implies Users acknowledgement and assumption of this risk
+          involved. Zoroz has made all reasonable efforts to put in place
+          reasonable physical, electronic, and managerial procedures to
+          safeguard the information we collect. We seek to ensure compliance
+          with the requirements of the Information Technology Act, 2000 as
+          amended and rules made there under to ensure the protection and
+          preservation of your privacy. However, it is your responsibility to
+          not provide any sensitive data that you would not want to make public.
+          You should be aware that your account may be monitored by the
+          customers and service providers across the Websites. We also use
+          cookies to administer our platforms and deliver a personalized
+          experience. For additional detailed information on each of these,
+          please see below.
+          <h6>Cookies</h6>
+          “Cookies” are pieces of information that may be placed on User’s
+          device by a service provider for the purpose of facilitating and
+          enhancing User communication and interaction. To the extent possible
+          depending upon User’s device, Zoroz may use cookies (and similar items
+          such as web beacons, tags, etc.) to customize User experience, to make
+          usage of Apps more convenient or to enable Zoroz to enhance its
+          Services. Zoroz may also use and place cookies on User devices from
+          our third party service providers in connection with the User
+          services, such as an analytics provider that helps User manage and
+          analyze Zoroz Services. User may stop or restrict the placement of
+          cookies on certain devices or flush them from User’s browser by
+          adjusting User’s web browser preferences. However, User may still use
+          Zoroz Services, but it may interfere with some of its functionality.
+          Cookies and similar items are not used by Zoroz, and may automatically
+          retrieve personal information from Users device without User’s
+          knowledge. Cookies are device and browser specific, so if User makes a
+          change to User’s environment Zoroz may reinstall a cookie. We might
+          utilize session ID cookies to enable certain features of the website,
+          to better understand how you interact with the website and to monitor
+          aggregate usage by Zoroz users and web traffic routing on the Website.
+          Unlike persistent cookies, session cookies are deleted from your
+          computer/ device when you log off from the website and close your
+          browser. Third party advertisers on the website may also place or read
+          cookies on your browser. You can instruct your browser, by changing
+          its options, to stop accepting cookies or to prompt you before
+          accepting a cookie from the website you visit. If you do not accept
+          cookies, however, you may not be able to use all portions of the
+          website or Services. We might utilize session ID cookies to enable
+          certain features of the website, to better understand how you interact
+          with the website and to monitor aggregate usage by Zoroz users and web
+          traffic routing on the Website. Unlike persistent cookies, session
+          cookies are deleted from your computer/ device when you log off from
+          the website and close your browser. Third party advertisers on the
+          website may also place or read cookies on your browser. You can
+          instruct your browser, by changing its options, to stop accepting
+          cookies or to prompt you before accepting a cookie from the website
+          you visit. If you do not accept cookies, however, you may not be able
+          to use all portions of the website or Services. We might also use
+          tracking technologies which may record information such as internet
+          domain and host names; internet protocol (IP) addresses; browser
+          software and operating system types; click stream patterns; dates and
+          times; and location from which the website is accessed. Our use of
+          cookies and other tracking technologies allows us to improve our web
+          site and your web experience. We may also analyze information that
+          does not contain Personal Information for Trends and statistics.
+          <h6>Changes To Privacy Policy</h6>
+          Zoroz, reserves the right, at its discretion, to change, modify, add,
+          or remove portions from this Privacy Policy at any time. However, if
+          at any time in the future Zoroz plans to use personal information or
+          location-based data in a way that materially differs from this Privacy
+          Policy, such as sharing such information with more third parties,
+          Zoroz, will post such changes here and provide Users the opportunity
+          to opt-out of such differing uses. Users, continuing to use our
+          services, following the posting of any changes to this Privacy Policy
+          means User’s acceptance to such changes.
+          <h6>DISCLAIMER:</h6>
+          WE RESERVE THE RIGHT TO CHANGE THE TERMS AND PRIVACY POLICY FROM TIME
+          TO TIME AS WE DEEM FIT, WITHOUT ANY PRIOR INTIMATION TO YOU. YOUR
+          CONTINUED USE OF THE WEB SITE SIGNIFIES YOUR ACCEPTANCE OF ANY
+          AMENDMENT TO THESE TERMS. YOU ARE THEREFORE ADVISED TO READ THE
+          PRIVACY POLICY ON A REGULAR BASIS. IN CASE YOU DISAGREE WITH ANY OF
+          THE TERMS AND PRIVACY POLICIES OR ANY AMENDMENTS THEREAFTER, YOU MAY
+          TERMINATE YOUR USE OF THIS WEBSITE IMMEDIATELY. WE FOLLOW GENERALLY
+          ACCEPTED STANDARDS TO PROTECT THE PERSONAL INFORMATION SUBMITTED TO
+          US, INCLUDING THE USE OF SERVICES FORM THIRD PARTY SERVICE PROVIDERS.
+          THEREFORE, WHILE WE STRIVE TO USE COMMERCIALLY ACCEPTABLE MEANS TO
+          PROTECT YOUR PERSONAL INFORMATION, WE CANNOT GUARANTEE ABSOLUTE
+          SECURITY AND THEREBY USAGE IN A MANNER THAT IS INCONSISTENT WITH THIS
+          PRIVACY POLICY. WE ASSUME NO LIABILITY OR RESPONSIBILITY FOR
+          DISCLOSURE OF YOUR INFORMATION DUE TO ERRORS IN TRANSMISSION,
+          UNAUTHORIZED THIRD-PARTY ACCESS, OR OTHER CAUSES BEYOND OUR CONTROL.
+          THE ONUS OF PROTECTING THE CLASSIFIED/ PERSONAL INFORMATION DETAILS OF
+          EVERY USER SOLELY DEPENDS ON THEM AND WE SHALL TAKE NO LIABILITY IN
+          CASE OF ANY BREACH THEREUPON. WE RECOMMEND YOU TO PROTECT YOUR
+          PERSONAL INFORMATION BY NEVER PROVIDING YOU CREDIT CARD, DEBIT OR ANY
+          OTHER BANK ACCOUNT DETAILS TO ANYONE. ZOROZ PROVIDES MAXIMUM CARE AS
+          POSSIBLE TO ENSURE THAT ALL OR ANY DATA / INFORMATION IN RESPECT OF
+          ELECTRONIC TRANSFER OF MONEY REMAIN SECURE. FOR COMPLETING ONLINE
+          TRANSACTIONS INVOLVING PAYMENTS A USER IS DIRECTED TO A SECURED
+          PAYMENT GATEWAY, ZOROZ/ DO NOT STORE OR KEEP FINANCIAL DATA SUCH AS
+          CREDIT CARD NUMBERS/PASSWORDS/PINS ETC OR ANY “PERSONAL ACCOUNT
+          RELATED INFORMATION” FOR ITS OWN USE. SINCE THE TRANSACTION HAPPENS ON
+          A THIRD PARTY NETWORK NOT CONTROLLED BY ZOROZ/ , ONCE AN ONLINE
+          TRANSACTION HAS BEEN COMPLETED, THE PERSONAL ACCOUNT RELATED
+          INFORMATION IS NOT ACCESSIBLE TO ANYONE AT ZOROZ/, AT THE PAYMENT
+          GATEWAY, AND THIS ENSURES MAXIMUM SECURITY. ZOROZ/ SHALL NOT BE LIABLE
+          FOR ANY LOSS OR DAMAGE SUSTAINED BY REASON OF ANY DISCLOSURE
+          (INADVERTENT OR OTHERWISE) OF ANY INFORMATION CONCERNING THE USER’S
+          ACCOUNT AND/ OR INFORMATION RELATING TO OR REGARDING ONLINE
+          TRANSACTIONS USING CREDIT CARDS / DEBIT CARDS AND/ OR THEIR
+          VERIFICATION PROCESS AND PARTICULARS NOR FOR ANY ERROR, OMISSION OR
+          INACCURACY WITH RESPECT TO ANY INFORMATION SO DISCLOSED AND USED
+          WHETHER OR NOT IN PURSUANCE OF A LEGAL PROCESS OR OTHERWISE. YOU PLAY
+          AN IMPORTANT ROLE IN KEEPING YOUR PERSONAL INFORMATION SECURE. YOU
+          SHOULD NOT SHARE YOUR USER NAME, PASSWORD, OR OTHER SECURITY
+          INFORMATION OF YOUR ZOROZ ACCOUNT WITH ANYONE. IF WE RECEIVE
+          INSTRUCTIONS USING YOUR USER NAME AND PASSWORD, WE WILL CONSIDER THAT
+          YOU HAVE AUTHORIZED THE INSTRUCTIONS.
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default page;
