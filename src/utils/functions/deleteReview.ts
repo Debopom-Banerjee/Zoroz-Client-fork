c8e581ecece655bc83a696ba7b1024ef4ae60b70
Soumyaraj Bag <soumyarajbag@gmail.com>
2024-05-26 15:21:43 +0530

    fix:thankyoumodal

--- src/utils/functions/deleteReview.ts
+++ src/utils/functions/deleteReview.ts
@@ -7,7 +7,7 @@ export const deleteReview = async(userId:string,productId:string)=>{
           const updatedInputs = { userId: userId, productId: productId };
           console.log(updatedInputs)
         const response = await axios.post(
-          `http://localhost:5000/products/reviews/delete`,
+          `https://zoroz-ecommerce-backend.onrender.com/products/reviews/delete`,
           updatedInputs,
           {
             headers: {
