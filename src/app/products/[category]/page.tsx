8cbf7314b3e780679e335cc2e4840d507fd632fa
Soumyaraj Bag <soumyarajbag@gmail.com>
2024-05-27 01:41:22 +0530

    add:price-filter

--- src/app/products/[category]/page.tsx
+++ src/app/products/[category]/page.tsx
@@ -9,40 +9,120 @@ import { PuffLoader } from "react-spinners";
 
 const page = () => {
   const [productData, setProductData] = useState<any>([]);
-  const categoryString = (useParams().category.toLocaleString());
-  console.log(categoryString)
+  const [maxPriceFilter, setMaxPriceFilter] = useState<number>(0);
+  const [minPriceFilter, setMinPriceFilter] = useState<number>(0);
+  const categoryString = useParams().category.toLocaleString();
   const category = decodeURIComponent(categoryString);
-  console.log(category)
+  const [filterProducts, setFilteredProducts] = useState<any>([]);
+  const [priceOptions, setPriceOptions] = useState<number[]>([0]);
   const [loading, setLoading] = useState(true);
   useEffect(() => {
     const fetchProducts = async () => {
+      setLoading(true);
       const data = await getProductsByCategory(categoryString);
       setProductData(data);
+      setFilteredProducts(data);
       console.log(data);
-      setLoading(false)
+
+      // Calculate the lowest and highest prices
+      if (data.length > 0) {
+        const prices = data.map((product: any) => product.price);
+        const minPrice = Math.floor(Math.min(...prices) / 100) * 100;
+        const maxPrice = Math.ceil(Math.max(...prices) / 100) * 100;
+        setMinPriceFilter(minPrice);
+        setMaxPriceFilter(maxPrice);
+        // Generate price options
+        const step = 400; // You can adjust this step value as needed
+        const options = [];
+        for (let price = minPrice; price <= maxPrice; price += step) {
+          options.push(price);
+        }
+        setPriceOptions(options);
+      }
+
+      setLoading(false);
     };
     fetchProducts();
-  }, [category]);
+  }, [categoryString]);
+
+  useEffect(() => {
+    const filteredData = () => {
+      const filtered = productData.filter((product: any) => {
+        const price = product.price;
+        const min = minPriceFilter || 0;
+        const max = maxPriceFilter || Infinity;
+        return price >= min && price <= max;
+      });
+      setFilteredProducts(filtered);
+    };
+
+    filteredData();
+  }, [minPriceFilter, maxPriceFilter, productData]);
   return (
     <>
-    <Navbar />
-    <div className="w-full mx-auto my-10">
-      <div className="flex flex-col bg-white items-start gap-5 mx-auto justify-center rounded-xl w-full px-5 py-5 lg:w-4/5 ">
-        <h1 className="font-semibold text-2xl">{category}</h1>
-        <div className="flex flex-row flex-wrap gap-5 md:gap-3 items-center self-center justify-center mx-auto w-full">
-          {loading ? 
-
-          <div className="min-h-[80vh] flex flex-col items-center justify-center ">
-   <PuffLoader size={35} color="black" />
+      <Navbar />
+      <div className="w-5/6 flex flex-col md:flex-row gap-5 items-start mx-auto my-10">
+        <div className="bg-white px-10 py-5 max-md:w-full mx-auto flex flex-col items-center justify-center  rounded-lg">
+          <h1 className="font-semibold text-lg">Filters</h1>
+          <div className="flex flex-col items-start gap-3">
+            <h1 className="font-semibold">Price:</h1>
+            <label>Lowest: </label>
+            <select
+              name="lowestPrice"
+              value={minPriceFilter}
+              onChange={(event: React.ChangeEvent<HTMLSelectElement>) => {
+                setMinPriceFilter(parseInt(event.target.value));
+              }}
+            >
+              <option value={0} defaultValue={0}>
+                Select
+              </option>
+              {priceOptions.map((price, index) => (
+                <option key={index} value={price}>
+                  {price}
+                </option>
+              ))}
+            </select>
+            <label>Highest: </label>
+            <select
+              name="highestPrice"
+              value={maxPriceFilter}
+              onChange={(event: React.ChangeEvent<HTMLSelectElement>) => {
+                setMaxPriceFilter(parseInt(event.target.value));
+              }}
+            >
+              <option value={0} defaultValue={0}>
+                Select
+              </option>
+              {priceOptions.map((price, index) => (
+                <option key={index} value={price}>
+                  {price}
+                </option>
+              ))}
+            </select>
           </div>
-       
-          : productData.map((product: any, index: number) => {
-            return <ProductView product={product} key={index} />;
-          })}
+        </div>
+        <div className="flex flex-col bg-white items-start gap-5 mx-auto justify-center rounded-xl w-full px-5 py-5  ">
+          <h1 className="font-semibold text-2xl">{category}</h1>
+          <div className="flex flex-row flex-wrap gap-5 md:gap-3 items-center self-center justify-center mx-auto w-full">
+            {loading ? (
+              <div className="min-h-[80vh] flex flex-col items-center justify-center ">
+                <PuffLoader size={35} color="black" />
+              </div>
+            ) : (
+              filterProducts.length > 0 ? 
+              filterProducts.map((product: any, index: number) => {
+                return <ProductView product={product} key={index} />;
+              })
+              :
+             <div className="flex flex-col items-center justify-center mx-auto">
+                <h1 className="font-semibold text-red-600 text-lg my-20">No Products Found !</h1>
+             </div>
+            )}
+          </div>
         </div>
       </div>
-    </div>
-    <Footer />
+      <Footer />
     </>
   );
 };
--- src/app/products/brands/[brands]/page.tsx
+++ src/app/products/brands/[brands]/page.tsx
@@ -8,41 +8,111 @@ import React, { useEffect, useState } from "react";
 import { PuffLoader } from "react-spinners";
 
 const page = () => {
-
   const [productData, setProductData] = useState<any>([]);
+  const [maxPriceFilter, setMaxPriceFilter] = useState<number>(0);
+  const [minPriceFilter, setMinPriceFilter] = useState<number>(0);
+  const [filterProducts, setFilteredProducts] = useState<any>([]);
+  const [priceOptions, setPriceOptions] = useState<number[]>([0]);
   const brand = useParams().brands.toLocaleString();
   console.log(brand);
   const [loading, setLoading] = useState(true);
   useEffect(() => {
     const fetchProducts = async () => {
       const data = await getProductsByBrand(brand);
-      console.log(data)
+      console.log(data);
       setProductData(data);
-
-      setLoading(false)
+      if (data.length > 0) {
+        const prices = data.map((product: any) => product.price);
+        const minPrice = Math.floor(Math.min(...prices) / 100) * 100;
+        const maxPrice = Math.ceil(Math.max(...prices) / 100) * 100;
+        setMinPriceFilter(minPrice);
+        setMaxPriceFilter(maxPrice);
+        // Generate price options
+        const step = 400; // You can adjust this step value as needed
+        const options = [];
+        for (let price = minPrice; price <= maxPrice; price += step) {
+          options.push(price);
+        }
+        setPriceOptions(options);
+      }
+      setLoading(false);
     };
     fetchProducts();
   }, [brand]);
+
+  useEffect(() => {
+    const filteredData = () => {
+      const filtered = productData.filter((product: any) => {
+        const price = product.price;
+        const min = minPriceFilter || 0;
+        const max = maxPriceFilter || Infinity;
+        return price >= min && price <= max;
+      });
+      setFilteredProducts(filtered);
+    };
+
+    filteredData();
+  }, [minPriceFilter, maxPriceFilter, productData]);
   return (
     <>
-    <Navbar />
-    <div className="w-full mx-auto my-10">
-      <div className="flex flex-col bg-white items-start gap-5 mx-auto justify-center rounded-xl w-full px-5 py-5 lg:w-4/5 ">
-        <h1 className="font-semibold text-2xl">{brand}</h1>
-        <div className="flex flex-row flex-wrap gap-5 md:gap-10 lg:gap-20 items-center self-center justify-center mx-auto w-full">
-          {loading ? 
-
-          <div className="min-h-[80vh] flex flex-col items-center justify-center ">
-   <PuffLoader size={35} color="black" />
+      <Navbar />
+      <div className="w-5/6 flex flex-col md:flex-row gap-5 items-start mx-auto my-10">
+        <div className="bg-white px-10 py-5 max-md:w-full mx-auto flex flex-col items-center justify-center  rounded-lg">
+          <h1 className="font-semibold text-lg">Filters</h1>
+          <div className="flex flex-col items-start gap-3">
+            <h1 className="font-semibold">Price:</h1>
+            <label>Lowest: </label>
+            <select
+              name="lowestPrice"
+              value={minPriceFilter}
+              onChange={(event: React.ChangeEvent<HTMLSelectElement>) => {
+                setMinPriceFilter(parseInt(event.target.value));
+              }}
+            >
+              <option value={0} defaultValue={0}>
+                Select
+              </option>
+              {priceOptions.map((price, index) => (
+                <option key={index} value={price}>
+                  {price}
+                </option>
+              ))}
+            </select>
+            <label>Highest: </label>
+            <select
+              name="highestPrice"
+              value={maxPriceFilter}
+              onChange={(event: React.ChangeEvent<HTMLSelectElement>) => {
+                setMaxPriceFilter(parseInt(event.target.value));
+              }}
+            >
+              <option value={0} defaultValue={0}>
+                Select
+              </option>
+              {priceOptions.map((price, index) => (
+                <option key={index} value={price}>
+                  {price}
+                </option>
+              ))}
+            </select>
           </div>
-       
-          : productData.map((product: any, index: number) => {
-            return <ProductView product={product} key={index} />;
-          })}
+        </div>
+        <div className="flex flex-col bg-white items-start gap-5 mx-auto justify-center rounded-xl w-full px-5 py-5 lg:w-full ">
+          <h1 className="font-semibold text-2xl">{brand}</h1>
+          <div className="flex flex-row flex-wrap gap-5 md:gap-10 lg:gap-3 items-center self-center justify-center mx-auto w-full">
+            {loading ? (
+              <div className="min-h-[80vh] flex flex-col items-center justify-center ">
+                <PuffLoader size={35} color="black" />
+              </div>
+            ) : (
+              productData.map((product: any, index: number) => {
+                return <ProductView product={product} key={index} />;
+              })
+            )}
+          </div>
         </div>
       </div>
-    </div>
-    <Footer />
+      <Footer />
     </>
   );
 };
